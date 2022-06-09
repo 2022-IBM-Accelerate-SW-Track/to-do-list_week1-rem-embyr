9b4d26acbdb9a380dfd9649ae6cea5efe4f86c6e
Embyr W <embyrwilliams@gmail.com>
2022-06-08 20:39:26 -0400

    Updated imports

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profilepic from "../assets/profilepic.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            profilepic
+            alt="Profile Pic"
+            ></img>
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title"><h4>Embyr Williams</h4></div>
+          <div className="brief_description">
+          A rising sophomore at Columbia University, Embyr is majoring in computer science with a concentration in visual arts. She is the secretary of Columbia Wushu and has her black belt in Taekwondo.
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
